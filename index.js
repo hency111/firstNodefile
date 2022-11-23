ca35e590230c7e70d4006108532e71419ceefdab
piyush <piyush0101nakarani@gmail.com>
2022-11-23 09:10:42 +0530

    dd

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const   mongoose = require('mongoose');
 
-const port =  process.env.PORT || 8081;
+
 const app = express();
 
 const path = require('path');
 
+
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+app.use(express.urlencoded());
+app.use('/uploads', express.static(path.join(__dirname,'/uploads')));
+app.use('/',require('./routes'));
 mongoose.connect("mongodb+srv://jaydeepCollege:jaydeep@123@cluster0.36fufoj.mongodb.net/college", {
     useNewUrlParser : true,
     useUnifiedTopology : true
@@ -15,14 +23,7 @@ mongoose.connect("mongodb+srv://jaydeepCollege:jaydeep@123@cluster0.36fufoj.mong
     console.log("db connected");
 });
 
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
-
-app.use(express.urlencoded());
-app.use('/uploads', express.static(path.join(__dirname,'/uploads')));
-app.use('/',require('./routes'));
-
-
+const port =  process.env.PORT || 8081;
 app.listen(port,function(err){
     if(err){
         console.log("server is not run");
