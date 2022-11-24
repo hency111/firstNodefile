bc7d9b27976fbb4c5cb883499821b17c8d444caa
piyush <piyush0101nakarani@gmail.com>
2022-11-24 08:42:53 +0530

    change db password

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const   mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 
 const app = express();
@@ -15,12 +15,19 @@ app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded());
 app.use('/uploads', express.static(path.join(__dirname,'/uploads')));
 app.use('/',require('./routes'));
-mongoose.connect("mongodb+srv://jaydeepCollege:jaydeep@123@cluster0.36fufoj.mongodb.net/college", {
+mongoose.connect("mongodb+srv://jaydeepCollege:Sg9rbcTGEIuv1c4f@cluster0.36fufoj.mongodb.net/college?retryWrites=true&w=majority", {
     useNewUrlParser : true,
     useUnifiedTopology : true
 })
 .then(() => {
     console.log("db connected");
+});
+
+
+mongoose.connection.once('open', function(){
+    console.log("connection been made");
+}).on('error', function(error){
+    console.log("error is"+error)
 });
 
 const port =  process.env.PORT || 8081;
